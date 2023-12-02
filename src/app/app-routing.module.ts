f25c97fbeac2758184d7ca5e3b6ad61e5887ec29
edwinkay <edwinkaycut@gmail.com>
2023-12-02 13:30:21 -0500

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { MusicaComponent } from './components/musica/musica.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: 'musica', component: MusicaComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { MusicaComponent } from './components/musica/musica.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    GaleriaComponent,
+    VideosComponent,
+    MusicaComponent
   ],
   imports: [
     BrowserModule,
