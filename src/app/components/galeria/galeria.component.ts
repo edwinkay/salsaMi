9480ed7558a63d475556d92d09b420d051f90825
edwinkay <edwinkaycut@gmail.com>
2023-12-03 13:38:08 -0500

    version 4

--- src/app/components/galeria/galeria.component.ts
+++ src/app/components/galeria/galeria.component.ts
@@ -1,39 +1,99 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage'
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+  AnimationEvent,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
-  styleUrls: ['./galeria.component.scss']
+  styleUrls: ['./galeria.component.scss'],
 })
 export class GaleriaComponent implements OnInit {
+  images: string[] = [];
+  showCount = false;
 
-  images: string[] = []
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage = this.images[0];
+  currentIndex = 0;
+  controls = true;
+  totalImageCount = 0;
+  reset = 0;
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage, private el: ElementRef) {}
 
   ngOnInit(): void {
-    this.getImages()
+    this.getImages();
   }
 
-  subirArchivo($event: any){
-    const file = $event.target.files[0]
+  subirArchivo($event: any) {
+    const file = $event.target.files[0];
 
-    const imgRef = ref(this.storage, `images/${file.name}`)
- 
-    uploadBytes(imgRef, file).then(x => {
-      this.getImages()
-    }).catch(error => console.log(error))
+    const imgRef = ref(this.storage, `images/${file.name}`);
+    uploadBytes(imgRef, file)
+      .then((x) => {
+        this.getImages();
+      })
+      .catch((error) => console.log(error));
   }
   getImages() {
-    const imagesRef = ref(this.storage, 'images')
+    const imagesRef = ref(this.storage, 'images');
 
-    listAll(imagesRef).then(async images => {
-      this.images = []
-      for(let image of images.items){
-        const url = await getDownloadURL(image)
-        this.images.push(url)
-      }
-    }).catch(error => console.log(error))
+    listAll(imagesRef)
+      .then(async (images) => {
+        this.images = [];
+        for (let image of images.items) {
+          const url = await getDownloadURL(image);
+          this.images.push(url);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  //insert function lightbox
+
+  onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.showCount = true;
+    this.currentLightboxImage = this.images[index];
+    this.totalImageCount = this.images.length;
+    console.log(this.totalImageCount);
+  }
+  onClosePreview() {
+    this.previewImage = false;
+    this.showMask = false;
+  }
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if (this.currentIndex > this.images.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.images.length - 1;
+    }
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+
+  onMouseEnter(index: number) {
+    const imageElement =
+      this.el.nativeElement.querySelectorAll('.zoomable-image')[index];
+    imageElement.style.transform = 'scale(1.1)';
+  }
+
+  onMouseLeave(index: number) {
+    const imageElement =
+      this.el.nativeElement.querySelectorAll('.zoomable-image')[index];
+    imageElement.style.transform = 'scale(1)';
   }
 }
