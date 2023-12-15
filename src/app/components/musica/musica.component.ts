119a2ccb201c14465a5ce71b3cdf062722f1321c
edwinkay <edwinkaycut@gmail.com>
2023-12-15 11:57:30 -0500

    version 6

--- src/app/components/musica/musica.component.ts
+++ src/app/components/musica/musica.component.ts
@@ -1,15 +1,163 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Storage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+} from '@angular/fire/storage';
+
+interface Music {
+  title: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-musica',
   templateUrl: './musica.component.html',
-  styleUrls: ['./musica.component.scss']
+  styleUrls: ['./musica.component.scss'],
 })
 export class MusicaComponent implements OnInit {
+  musica: Music[] = [];
+  titulos: string[] = [
+    'Aguanile',
+    'Arrepientete',
+    'Asi soy yo',
+    'Bam bam bidi bim bam bam',
+    'Batman y el hombre Araña',
+    'Caderona',
+    'Con la punta del pie',
+    'Donde estabas tu',
+    'Welcome to party',
+    'La negra tiene tumbao',
+    'Llora Corazon',
+    'Payaso',
+    'Cancion 13',
+    'Lupita',
+    'Paloma',
+    'Yo soy el rey',
+    'Cancion 17',
+    'Sangre son colora',
+    'Sangre son colora (editado)',
+    'boogalo 1',
+    'Cancion 21',
+    'Cancion 22',
+    'Cancion 23',
+    'Todo tiene su final (editado)',
+    'Cancion 25',
+    'Cancion 26',
+    'Cancion 27',
+    'Omele',
+    'Boogalo 2',
+    'Aguazate',
+    'Aqui no ha pasado nada',
+    'Saraguey santoja',
+    'Arranca en fa',
+    'Arroyando',
+    'Asi soy yo (edit)',
+    'Askarakatiski',
+    'Ballandel',
+    'Bam bam (edit)',
+    'Bomba caranbomba',
+    'Boogalo 3',
+    'Boogalo 4',
+    'Boogalo 5',
+    'Boogalo 6',
+    'Cancion 45',
+    'Boogalo 7',
+    'Boogalo 8',
+    'Boogalo 9',
+    'Boogalo 10',
+    'Boogalo 11',
+    'Boogalo 12',
+    'Boogalo 13',
+    'Cancion 53',
+    'Cachondea',
+    'Cancion 54',
+    'Camina como chencha',
+    'Cancion 56',
+    'Carmen',
+    'Cancion 57',
+    'Cancion 58',
+    'Cancion 59',
+    'Cancion 60',
+    'Cancion 61',
+    'Ran kan kan',
+    'Echo chi echo no',
+    'El malo',
+    'Cancion 65',
+    'Federico boogalo',
+    'Fuego pal 23',
+    'Hagan silencio',
+    'Indestructible',
+    'Lancion 70',
+    'cancion 71',
+    'La bamba',
+    'Cancion 73',
+    'La luz del cielo',
+    'La pelota',
+    'Cancion 76',
+    'Mambo rock',
+    'Merecumbe',
+    'Boogalo 14',
+    'Boogalo 15',
+    'Mondongo',
+    'Oh mayi',
+    'Pal 23',
+    'Cancion 85',
+    'Pruebalo',
+    'Quimbara (edit)',
+    'Quimbara',
+    'Cancion 89',
+    'Cancion 90',
+    'Cancion 91',
+    'Cancion 92',
+    'Cancion 93',
+    'Fuma el barco',
+    'Sonido bestial',
+    'Cancion 96',
+    'Cancion 97',
+    'Cancion 98',
+    'Cancion 99',
+    'El yoyo',
+    'Tremendo rumbon',
+    'Tres pata',
+    'Tres pata v2',
+    'Cancion 104',
+    'Cancion 105',
+  ];
 
-  constructor() { }
+  constructor(private storage: Storage) {}
 
   ngOnInit(): void {
+    this.getMusica();
   }
 
+  subirArchivo($event: any) {
+    const file = $event.target.files[0];
+
+    const imgRef = ref(this.storage, `videos/${file.name}`);
+    uploadBytes(imgRef, file)
+      .then(() => {
+        this.getMusica();
+      })
+      .catch((error) => console.log(error));
+  }
+
+  async getMusica() {
+    const imagesRef = ref(this.storage, 'musica');
+
+    try {
+      const music = await listAll(imagesRef);
+      this.musica = await Promise.all(
+        music.items.map(async (item, index) => {
+          const url = await getDownloadURL(item);
+          const title = this.titulos[index] || `Canción ${index + 1}`;
+          return { title, url };
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Storage, ref, uploadBytes, listAll, getDownloadURL,
+import {
+  Storage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
 } from '@angular/fire/storage';
+
+interface Video {
+  title: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-videos',
@@ -8,36 +18,57 @@ import { Storage, ref, uploadBytes, listAll, getDownloadURL,
   styleUrls: ['./videos.component.scss'],
 })
 export class VideosComponent implements OnInit {
-  videos: string[] = [];
+  videos: Video[] = [];
+  titulos: string[] = [
+    'Ensayo, 1',
+    'Cecedit',
+    'Ensayo, 2',
+    'Ensayo, 3',
+    'Parque corinto',
+    'Casa de la cultura',
+    'Padilla',
+    'Parque corinto v2',
+    'Nucleo corinto',
+    'Ensayo 4',
+    'Ensayo 5',
+    'Ensayo 6',
+    'Nucleo',
+    'Casa de la cultura v2',
+    'Happy birthday',
+    'Algun lugar',
+  ];
 
   constructor(private storage: Storage) {}
 
   ngOnInit(): void {
-    this.getVideos()
+    this.getVideos();
   }
 
   subirArchivo($event: any) {
     const file = $event.target.files[0];
 
-    const imgRef = ref(this.storage, `videos/${file.name}`);
-    uploadBytes(imgRef, file)
-      .then((x) => {
+    const videoRef = ref(this.storage, `videos/${file.name}`);
+    uploadBytes(videoRef, file)
+      .then(() => {
         this.getVideos();
       })
       .catch((error) => console.log(error));
   }
-  getVideos() {
-    const imagesRef = ref(this.storage, 'videos');
-    console.log(imagesRef)
 
-    listAll(imagesRef)
-      .then(async (videos) => {
-        this.videos = [];
-        for (let video of videos.items) {
+  async getVideos() {
+    const videosRef = ref(this.storage, 'videos');
+
+    try {
+      const videoList = await listAll(videosRef);
+      this.videos = await Promise.all(
+        videoList.items.map(async (video, index) => {
           const url = await getDownloadURL(video);
-          this.videos.push(url);
-        }
-      })
-      .catch((error) => console.log(error));
+          const title = this.titulos[index] || `Video ${index + 1}`;
+          return { title, url };
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
